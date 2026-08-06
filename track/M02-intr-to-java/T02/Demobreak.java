8516e371c3ff5f68ad09c85ce355867363c35977
Charmi <charmipasala@gmail.com>
2026-08-06 13:10:12 +0530

    Demobreak

---
+++ track/M02-intr-to-java/T02/Demobreak.java
@@ -0,0 +1,16 @@
+
+public class Demobreak {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 3; i++) {
+            for (int j = 0; j <= 3; j++) {
+                if (j == 2) {
+                    break;
+                }
+                System.out.println("i:" + i + "j:" + j);
+            }
+
+        }
+    }
+
+}
---
+++ track/M02-intr-to-java/T02/Lable_Nested.java
@@ -0,0 +1,6 @@
+public class Lable_Nested {
+    public static void main(String[] args){
+
+    }
+        
+    }
