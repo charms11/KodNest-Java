f9d78dfbd89ab225f6d677ff6fa9197fa11299c3
Charmi <charmipasala@gmail.com>
2026-08-11 12:37:57 +0530

    Employee

---
+++ track/M02-intr-to-java/T04/Employee.java
@@ -0,0 +1,29 @@
+
+public class Employee {
+
+    int id;
+    String name;
+
+    void work() {
+        System.out.println("Working");
+    }
+
+    public static void main(String[] args) {
+        Employee e1 = new Employee();
+        e1.id = 15;
+        e1.name = "Pond";
+        System.out.println(e1.id);
+        System.out.println(e1.name);
+        e1.work();
+
+        Employee e2;
+        e2 = e1;
+        e2.id = 16;
+        e2.name = "Phuwin";
+
+        System.out.println(e1.id);
+        System.out.println(e2.id);
+        System.out.println(e1.name);
+        System.out.println(e2.name);
+    }
+}
