e91fa207570a52079174307a11e015a2e43d9a40
Charmi <charmipasala@gmail.com>
2026-08-14 12:57:49 +0530

    Constructor

--- track/M02-intr-to-java/T05/Constructor.java
+++ track/M02-intr-to-java/T05/Constructor.java
@@ -5,21 +5,10 @@ public class Constructor {
     int age;
     double height;
 
-    public Constructor() {
-        name = "Charmi";
-        age = 20;
-        height = 5.5;
-    }
-
     public Constructor(String name, int age, double height) {
         this.name = name;
         this.age = age;
         this.height = height;
-    }
-
-    public Constructor(String name) {
-        this.name = name;
-
     }
 
     public void display() {
@@ -29,12 +18,10 @@ public class Constructor {
     }
 
     public static void main(String[] args) {
-        Constructor c1 = new Constructor();
-        c1.display();
+
         Constructor c2 = new Constructor("Phuwin", 21, 5.8);
         c2.display();
-        Constructor c3 = new Constructor("Pond");
-        c3.display();
+
     }
 
 }
